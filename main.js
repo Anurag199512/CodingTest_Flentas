90dc1fa436554988c725a3f714aea57bbf13d1f0
Anurag199512 <anurag199512@gmail.com>
2020-01-18 12:45:06 +0530

    final version of code

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+//calculates the minimum travel cost
+function findCost(cost){
+    var total=0;
+
+    if(cost.length>3){
+        total=cost[0]+cost[cost.length-1]+(2*cost[1]<cost[0]+cost[cost.length-2]?2*cost[1]:cost[0]+cost[cost.length-2])
+
+        return total+findCost(cost.splice(0,cost.length-2))
+    }
+    else{
+        if(cost.length==2)
+            return (cost[0]>cost[1]?cost[0]:cost[1])
+        else if(cost.length==3)
+            return (cost[0]+cost[1]+cost[2])
+        else 
+            return cost[0]
+    }
+}
 
 //starting position of values from cmd line
 let pos=2;
@@ -9,20 +27,19 @@ pos=pos+1;
 while(n){
     //will store the cost of travelling for each person
     let cost=[];
-    let total=0;
-
+    
     //no of person
     let person=Number(process.argv[pos]);
     pos=pos+1;
-
+    let total=0;
     //inserting into the array
     while(person){
         cost.push(Number(process.argv[pos]));
         pos=pos+1;
         person=person-1;
     }
- 
-    //ordered in ascending order for cost
+
+    //sorting the array in ascending order
     cost.sort((a,b)=>a>b?1:-1)
 
     if(cost.length==0)
@@ -34,25 +51,11 @@ while(n){
     else if(cost.length==3)
         total=total+(cost[0]+cost[1]+cost[2])
 
-    else{   
-        let onleft=new Array(cost.length).fill(0)
-        console.log(onleft)
-        for(let i =0;i<cost.length;++i){
-
-            total=total+cost[i];
-
-            if(cost.length%2==0){
-
-            }
-            else{
-                    
-            }
-        }
+    else{ 
+        total=findCost(cost)
     }
-    
-    console.log(total);
-    n=n-1;
+
+    console.log(total)
+    n=n-1
+
 }
-
-
-
